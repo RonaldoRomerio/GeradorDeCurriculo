7e5975cd8eb36dcea7ce87fc2759d3285a3e105a
RonaldoRomerio <ronaldo.romeriosj@gmail.com>
2021-06-12 09:51:22 -0300

    Ajuste do Container para se adequar ao tamanho do menu

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,28 @@
 import React, {useContext} from 'react';
 import {AuthContext} from '../../context/auth';
 import Header from '../../Components/Header';
-import Menu from '../../Components/Menu'
+import Menu from '../../Components/Menu';
+import Card from '../../Components/Card';
+import {FiUser, FiGlobe} from 'react-icons/fi';
+import './style.css'
 export default function Dashboard() {
   return (
     <div>
         <Header/>
         <Menu/>
+        <div className='content'>
+          <div className='content_dashboard'>
+            <div className='container_card'>
+            <Card link='/candidatos' titulo='Qtd. de Candidatos'>
+              <span>0</span> <FiUser size={50} color={'red'}/>
+            </Card>
+            <Card link='/empresas' titulo='Qtd. de Empresas'>
+              <span>0</span> 
+              <FiGlobe size={50} color={'red'}/>
+            </Card>
+            </div>  
+          </div>
+        </div>
     </div>
   );
 }
